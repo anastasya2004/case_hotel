925cac9e1adca00b7c4318f6daeb48be1aba40aa
Sampli33 <maxxset@yandex.ru>
2024-04-22 20:57:06 +0700

    some adds

--- Room.py
+++ Room.py
@@ -1,6 +1,7 @@
 class Room:
     comfort_factor = {'стандарт': 1, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
     room_price = {'одноместный': 2900, 'двухместный': 2300, 'полулюкс': 3200, 'люкс': 4100}
+    food_type = {'без питания': 0, 'завтрак': 280, 'полупансион': 1000}
     all_rooms = []
 
     def __init__(self, txt_info):
@@ -14,12 +15,12 @@ class Room:
         Room.all_rooms.append(self)
 
     '''
-    недоделанный метод выбора комнаты под параметры клиента
+    недоделанный метод выбора комнаты под параметры клиента,
+    нужно будет разобраться что делать, если количество гостей = 4
     '''
     @classmethod
-    def room_selection(cls, guests, rental_days, price):
-        suitable_iter_1 = []
-        for room in Room.all_rooms:
-            if room.capacity == guests:
-                suitable_variants.append(room)
+    def room_selection(cls, guests, rental_days, price=None):
+        suitable_iter_1 = [room for room in Room.all_rooms if room.capacity == guests            # в этой переменной хранятся комнаты,
+                           and not any(dates in rental_days for dates in room.occupied_dates)]   # которые подходят по кол-ву человек и не заняты на желаемые даты
 
+        return suitable_iter_1
--- main.py
+++ main.py
@@ -24,12 +24,15 @@ analyzed_dates = Client.dates_to_modeling()
 Для каждой комнаты в атрибуте occupied_dates будет храниться занятость комнаты
 Даты желаемые клиентом будут сопоставляться с датами для каждой комнаты.
 Таким образом будет реализована проверка на доступность комнаты.
-Следующим шагом нужно реализовать подбор комнаты в отдельном методе класса Room or Client
+Следующим шагом нужно реализовать подбор комнаты в отдельном методе класса Room
 со всеми 
 '''
 for day in analyzed_dates:
     day_clients = [client for client in Client.all_clients if client.booking_date == day]
     for certain_client in day_clients:
-
-
-
+        chosen_room = Room.room_selection(certain_client.guests, certain_client.rental_days())
+        for i in chosen_room:
+            print(i.number)
+        break
+    break
+#последний цикл for написан просто для проверки правильности работы метода
